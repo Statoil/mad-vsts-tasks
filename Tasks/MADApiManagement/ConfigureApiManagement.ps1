113674b2041ab4f5f39d4a7f1676a3b88d368865
Frode Hus <frode.hus@outlook.com>
2017-03-14 16:45:11 +0100

    improved automation of certificate credentials

--- Tasks/MADApiManagement/ConfigureApiManagement.ps1
+++ Tasks/MADApiManagement/ConfigureApiManagement.ps1
@@ -1,32 +1,78 @@
 Trace-VstsEnteringInvocation $MyInvocation
-$webapp = Get-VstsInput -Name WebAppName -Require
 $apiManagement = Get-VstsInput -Name ApiManagement -Require
 $resourceGroupName = Get-VstsInput -Name ResourceGroupName -Require
-$apiName = Get-VstsInput -Name ApiName -Require
-$apiPath = Get-VstsInput -Name ApiPath -Require
 $productName = Get-VstsInput -Name ApiProduct -Require
-$useCredentials = Get-VstsInput -Name UseProxyCredentials -AsBool
-
+$action = Get-VstsInput -Name action -Require
 try{
     Import-Module $PSScriptRoot\ps_modules\VstsAzureHelpers_
+
     Initialize-Azure
 
     . "$PSScriptRoot\Util.ps1"
 
     $global:ErrorActionPreference = 'Stop'
     $global:__vstsNoOverrideVerbose = $true
-    Write-Host "##[command]Configuring API Management with API $apiName [$apiPath] for $webapp"
-    $api = Configure-ApiUsingSwaggerUrl -WebApp $webapp -ApiManagementName $apiManagement -ResourceGroupName $resourceGroupName -ApiName $apiName -ApiPath $apiPath
+    
+    Write-Host "==== $action ===="
     $ctx = New-AzureRmApiManagementContext -ResourceGroupName $resourceGroupName -ServiceName $apiManagement
     $product = Get-AzureRmApiManagementProduct -Context $ctx -Title $productName
-    
-    if(!$product) {
-        Write-Host "##[command]$productName does not exist - creating..."
-        $product = New-AzureRmApiManagementProduct -Context $ctx -Title $productName -SubscriptionRequired $false -ApprovalRequired $false -State Published
-    }
 
-    Write-Host "##[command]Adding API to product $($product.Title)"    
-    $result = Add-AzureRmApiManagementApiToProduct -Context $ctx -ProductId $product.ProductId -ApiId $api.ApiId
+    if($action -eq "Create Or Update API definition") {
+
+        $webapp = Get-VstsInput -Name WebAppName -Require
+        $apiName = Get-VstsInput -Name ApiName -Require
+        $apiPath = Get-VstsInput -Name ApiPath -Require
+        $useCredentials = Get-VstsInput -Name UseProxyCredentials -AsBool
+        $certificateThumbprint = Get-VstsInput -Name Certificates
+
+        Write-Host "##[command]Configuring API Management with API $apiName [$apiPath] for $webapp"
+        $api = Configure-ApiUsingSwaggerUrl -Context $ctx -WebApp $webapp -ApiName $apiName -ApiPath $apiPath
+        
+        if(!$product) {
+            throw (New-Object System.Exception("Product '$productName' does not exist"))
+        }
+
+        Write-Host "##[command]Adding API to product $($product.Title)"    
+        
+        $result = Add-AzureRmApiManagementApiToProduct -Context $ctx -ProductId $product.ProductId -ApiId $api.ApiId
+        $state = [Microsoft.Azure.Commands.ApiManagement.ServiceManagement.Models.PsApiManagementProductState]"Published"
+        $product = Set-AzureRmApiManagementProduct -Context $ctx -ProductId $product.ProductId -Description $description -ApprovalRequired $approvalRequired -SubscriptionRequired $subscriptionRequired -State $state
+        
+        if($useCredentials){
+            $certificate = Get-AzureRmApiManagementCertificate -Context $ctx | Where-Object {$_.Thumbprint -eq $certificateThumbprint}
+            if(!$certificate){
+                throw (New-Object System.Exception("Certificate with thumbprint $certificateThumbprint doesn't exist"))
+            }
+            Set-CertificateAuthentication -Context $ctx -ApiId $api.ApiId -Thumbprint $certificate.Thumbprint
+        }    
+    }
+    else {
+        $product = Get-AzureRmApiManagementProduct -Context $ctx -Title $productName
+        $description = Get-VstsInput -Name Description
+        $approvalRequired = Get-VstsInput -Name ApprovalRequired -AsBool
+        $subscriptionRequired = Get-VstsInput -Name SubscriptionRequired -AsBool
+        $useCredentials = Get-VstsInput -Name UseCertificateCredential -AsBool
+
+        
+        if(!$product) {
+            Write-Host "##[command]$productName does not exist - creating..."
+            $product = New-AzureRmApiManagementProduct -Context $ctx -Title $productName -Description $description -SubscriptionRequired $subscriptionRequired -ApprovalRequired $approvalRequired
+        }
+        else {
+            Write-Host "##[command]Updating $productName"
+            if($subscriptionRequired){
+                $product = Set-AzureRmApiManagementProduct -Context $ctx -ProductId $product.ProductId -Description $description -ApprovalRequired $approvalRequired -SubscriptionRequired $subscriptionRequired
+            }
+            else{
+                $product = Set-AzureRmApiManagementProduct -Context $ctx -ProductId $product.ProductId -Description $description -SubscriptionRequired $subscriptionRequired                
+            }
+        }
+
+        if($useCredentials) {
+            Get-AzureRmApiManagementCertificate -Context $ctx | Where-Object {$_.Thumbprint -eq $certificate}
+        }
+    }
+    
 }
 catch {
     Write-VstsTaskError -Message $_.Exception.Message
--- Tasks/MADApiManagement/Util.ps1
+++ Tasks/MADApiManagement/Util.ps1
@@ -1,24 +1,22 @@
 function Configure-ApiUsingSwaggerUrl {
     param(
         [string]$WebApp,
-        [string]$ApiManagementName,
-        [string]$ResourceGroupName,
+        [Microsoft.Azure.Commands.ApiManagement.ServiceManagement.Models.PsApiManagementContext]$Context,
         [string]$ApiName,
         [string]$ApiPath
     )
 
-    $ctx = New-AzureRmApiManagementContext -ResourceGroupName $ResourceGroupName -ServiceName $ApiManagementName
-    $api = Get-AzureRmApiManagementApi -Context $ctx -Name $ApiName
+    $api = Get-AzureRmApiManagementApi -Context $Context -Name $ApiName
 
     Update-SwaggerAndSaveToFile -WebApp $WebApp -ApiName $ApiName
     if($api){
         Write-Host "`tUpdating existing API at $ApiPath"
-        $api = Import-AzureRmApiManagementApi -Context $ctx -ApiId $api.ApiId -SpecificationFormat "Swagger" -SpecificationPath "$PSScriptRoot\swagger.json" -Path $ApiPath
+        $api = Import-AzureRmApiManagementApi -Context $Context -ApiId $api.ApiId -SpecificationFormat "Swagger" -SpecificationPath "$PSScriptRoot\swagger.json" -Path $ApiPath
     }
     else{
         Write-Host "`tCreating new API at $ApiPath"     
         $apiId = [guid]::NewGuid()   
-        $api = Import-AzureRmApiManagementApi -Context $ctx -ApiId $apiId -SpecificationFormat "Swagger" -SpecificationPath "$PSScriptRoot\swagger.json" -Path $ApiPath
+        $api = Import-AzureRmApiManagementApi -Context $Context -ApiId $apiId -SpecificationFormat "Swagger" -SpecificationPath "$PSScriptRoot\swagger.json" -Path $ApiPath
     }
     return $api
 }
@@ -36,4 +34,50 @@ function Update-SwaggerAndSaveToFile {
     $swagger | Add-Member -MemberType NoteProperty -Name "host" -value $hostname
     $swagger | Add-Member -MemberType NoteProperty -Name "schemes" -value @("https")
     $swagger | ConvertTo-Json -Depth 100 | Out-File "$PSScriptRoot\swagger.json"
+}
+
+function Set-CertificateAuthentication {
+    param(
+        [Microsoft.Azure.Commands.ApiManagement.ServiceManagement.Models.PsApiManagementContext]$Context,
+        [string]$ApiId,
+        [string]$Thumbprint
+    )
+    if(!$Context){
+            throw (New-Object System.Exception("No context available"))
+    }
+    Write-Host "##[command]Set-CertificateAuthentication"    
+
+    $policy = [xml](Get-AzureRmApiManagementPolicy -Context $Context -ApiId $ApiId)
+    if(!$policy){
+        Write-Host "`tNo policy found - setting default policy"
+        $policy = New-Policy
+    }
+
+    $certNode = $policy.policies.inbound.ChildNodes | Where-Object {$_.Name -eq "authentication-certificate"}
+    if($certNode) {
+        Write-Host "`tUpdating thumprint on existing policy"
+        $certNode.SetAttribute("thumbprint", $Thumbprint)
+    }
+    else {
+        Write-Host "`tAdding authentication policy"
+        $certNode = $policy.CreateElement("authentication-certificate");
+        $certNode.SetAttribute("thumbprint", $Thumbprint)
+    
+        $inbound = $policy.policies.ChildNodes | Where-Object {$_.Name -eq "inbound"}        
+        $inbound.AppendChild($certNode)
+    }
+    Set-AzureRmApiManagementPolicy -Context $Context -ApiId $ApiId -Policy $policy.OuterXml
+}
+
+function New-Policy {
+    $policy = @"
+    <policies>
+        <inbound />
+        <backend>
+                <forward-request />
+        </backend>
+        <outbound />
+</policies>
+"@
+    return [xml]$policy
 }
